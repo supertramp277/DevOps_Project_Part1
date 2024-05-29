23b0840c75ed5b4d2384b6107f8aa368211e71c0
yanlong wang <18621056089@163.com>
2024-05-29 21:36:19 +0200

    change to expect

--- test/test_matrix_multiplication.cpp
+++ test/test_matrix_multiplication.cpp
@@ -147,6 +147,27 @@ TEST(MatrixMultiplicationTest, TestMatrixWithLargeSize) {
     EXPECT_EQ(C, expected) << "Matrix multiplication test failed! :(((()";
 }
 
+//Test Case 8: Let's see the results of right multiplication
+TEST(MatrixMultiplicationTest, TestRightMultiplication) {
+    std::vector<std::vector<int>> A = {
+        {1, 2, 3},
+        {4, 5, 6}            
+    };                        
+    std::vector<std::vector<int>> B = {            
+        {7, 8},                        
+        {9, 10},                        
+        {11, 12}
+    };
+    std::vector<std::vector<int>> C(2, std::vector<int>(2, 0));
+
+    std::vector<std::vector<int>> expected = {
+        {58, 64},
+        {139, 154}
+    };
+
+    EXPECT_EQ(expected, expected) << "Matrix multiplication test failed! :(((()";
+}
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
